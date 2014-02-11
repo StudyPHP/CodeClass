485c47d7742043574484cd5468a091782189fbf5
Robert <robert.bogach@gmail.com>
2014-02-11 11:36:08 +0200

    Add function ShowForm for registere User

--- Personal/class.php
+++ Personal/class.php
@@ -15,7 +15,7 @@ class Personal extends DB
     {
         $this->option = parent::Where('login','=','$_POST["login"]');
 	return $option;
-	$this->data = parent::Select('pass','user',$this->option);
+	$this->data = parent::Select('user','pass',$this->option);
         return $data;
         $this->valid = mysql_query($this->data);
         if($this->valid)
@@ -40,5 +40,35 @@ class Personal extends DB
     function UserRegistere()
     {
         //
+    }
+    
+    function ShowForm () 
+    /*
+    Need to create a table "registration_form" in MySQL for the generate of the registration form
+    Field:
+	    name
+	    type
+	    placeholder
+	    required
+	    pattern
+	    coment
+     */
+    {
+    	$this->array = parent::Select('registration_form');
+        return $array; // ?...
+    	for ($i=0;$i<count($this->array['name'])-1;$i++)
+	{
+        	echo '<input type="'.$this->array['type'][$i].'" name="'.$this->array['name'][$i].'" placeholder="'.$this->array['placeholder'][$i].'" '.$this->array['required'][$i].' pattern="'.$this->array['pattern'][$i].'">  '.$this->array['coment'][$i];
+		if ($this->array['required'][$i] == 'required')
+		{
+        		echo ' *';
+		}
+        	echo '<br/>';
+    	}
+    	if (in_array ('required',$this->array['required']))
+    	{
+        	echo '<p>* - поля, обязательные для заполнения </p>';
+    	}
+    	echo '<p><input type="submit" name="'.end ($this->array['name']).'" value="'.end ($this->array['coment']).'">   <input type="reset" value="Очистить"></p>'; 
     }
 }
