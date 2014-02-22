ff456e96048268cbb1c5689ebe4efa7821c7f3a1
ulker <ulker85@inbox.ru>
2014-02-22 18:39:58 +0200

    modified class DB
    not working without " in queries

--- include/DB.php
+++ include/DB.php
@@ -2,20 +2,21 @@
 class DB {
     private $host = "localhost";
     private $user = "root";
-    private $db_name = "school_db";
     private $pass = "";
+    private $db_name = "school_db";
     public $conn;
+    
     public function __construct() {
        $this->conn = mysql_connect($this->host, $this->user, $this->pass);
        mysql_select_db($this->db_name);
     }
-    function Select($table,$row="*",$option=false)
-    {
+    
+    function Select($table, $row="*", $option=false) {
       if(!$option){
          $sql = "SELECT $row FROM $table"; 
       }
       if($option){
-         $sql = "SELECT $row FROM $table $option"; //убрал WHERE - лишня
+         $sql = "SELECT $row FROM $table".$option;
       }
       $data = mysql_query($sql);
       while($row = mysql_fetch_assoc($data)){
@@ -24,61 +25,55 @@ class DB {
       return $array;
     }
     
-    function Insert($table,$row,$value)
-    {
+    function Insert($table, $row, $value) {
        $str_row = join(", ", $row);
        $str_value = join(", ", $value);
        
        $sql = "INSERT INTO $table ($str_row) VALUE ($str_value)";
        $answer = mysql_query($sql);
+       
        return $answer;
-    } 
-    function Update ($table,$row,$value,$option=FALSE)
-    {
-       if (is_string($row) && is_string($value))
-       {
-           $sql = "UPDATE $table SET $row = $value";
+    }
+    
+    function Update($table, $row, $value, $option=FALSE) {
+       if (is_string($row) && is_string($value)) {
+           $sql = "UPDATE $table SET $row = '$value'";
        }
-       if (is_array($row) && is_array($value))
-       {
+       if (is_array($row) && is_array($value)) {
            $sql = "UPDATE $table SET ";
-           for ($i=0; $i < count($row); $i++){
-             $sql.="$row = $value".",";  
+           for ($i=0; $i < count($row); $i++) {
+             $sql.="$row = '$value'".",";  
            }
            $sql = substr($sql, 0, strlen($sql)-1);
        }
        $answer = mysql_query($sql);
-       return $answer;
-    }
-    function Delete($table,$option){
-       $sql = "DELETE FROM $table".$option;
-        
-       $answer = mysql_query($sql);
+       
        return $answer;
     }
     
-    function Where ($row, $value, $action)
-    {
-        $option = "";
+    function Delete($table, $option) {
+       $sql = "DELETE FROM $table".$option;        
+       $answer = mysql_query($sql);
+       
+       return $answer;
+    }
+    
+    function Where($row, $value, $action) {
+        $option = '';
+        
         if ($value)
-            $option = " WHERE $row $action $value"; //добавлен пробел перед WHERE
+            $option = " WHERE $row $action '$value'";
+        
         return $option;
     }
-    function Limit ($count)
-    {
+    
+    function Limit($count) {
         $option = '';
+        
         if(is_int($count))
-        $option = " LIMIT 0, $count"; //добавлен пробел перед LIMIT
+            $option = " LIMIT 0, $count";
+        
         return $option;
     }
 }
-
-
-
-
-//$connect = mysql_connect('localhost', 'root', '');
-//mysql_select_db('school_db');
-//
-//if (!$connect)
-//    echo 'Подключение к базе отсутствует';
 ?>
