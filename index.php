807eccf8a3e603af93069549bc7cecd27d2385b9
ulker <ulker85@inbox.ru>
2014-02-25 13:30:36 +0200

    creating branch

--- index.php
+++ index.php
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
 <?php
-include 'include.php';  
-$page = new Page ('page', 'Front Page');
-=======
-<?php include 'include.php';
- include 'controller.php';
+include 'include.php';
+include 'controller.php';
+
 $page = new Page ($page);
->>>>>>> 555f2bdcafefd9a6f23b2acb3eee80ead6e60c29
 $page->show();
--- page/class.php
+++ page/class.php
@@ -1,18 +1,6 @@
 <?php
 class Page extends Menu {
     public $type;
-<<<<<<< HEAD
-    public $data;
-    
-    public function __construct($type, $data) {
-        parent::__construct();
-        $this->type = $type;
-        $this->data = $data;
-    }
-    
-    function Show() {
-        include 'personal/personal.php';
-=======
     //public $data;
     
     public function __construct($type) {
@@ -21,7 +9,6 @@ class Page extends Menu {
         //$this->data = $data;
     }
     function show() {
->>>>>>> 555f2bdcafefd9a6f23b2acb3eee80ead6e60c29
         include 'template/header.tpl';
         $file = $this->type."/".$this->type.'.php';
         include $file;
